b4fc957cac6453cb526e9b747e46c793ed81de7c
ada682 <61917052+ada682@users.noreply.github.com>
2024-07-28 03:26:56 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,36 +1,17 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    // Scroll animation
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    function changeLinkState() {
+        let index = sections.length;
+
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    }
+
+    window.addEventListener('scroll', changeLinkState);
+    changeLinkState();
 });
-
-// Fade-in effect on scroll
-const faders = document.querySelectorAll('.fade-in');
-
-const appearOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -150px 0px"
-};
-
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        }
-    });
-}, appearOptions);
-
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-});
-
-function toggleMenu() {
-    document.querySelector('nav ul').classList.toggle('show');
-}
